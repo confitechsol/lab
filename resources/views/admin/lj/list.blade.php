32ab4aeb4e54cf3f06c2862db8c327fbe5a2bb74
saumen <info@xlinkinfocom.com>
2021-02-10 03:18:55 +0530

    new changes

--- resources/views/admin/lj/list.blade.php
+++ resources/views/admin/lj/list.blade.php
@@ -25,15 +25,12 @@
                   <div class="col-lg-12 col-xlg-12 col-md-12 col-sm-12" style="margin-top: 16px;">
                       <div class="card" style="border: none;">
                           <div class="card-block">
-
                             <form action="{{ url('/dashboardlj') }}" method="post">
                               <input type="hidden" name="_token" value="{{ csrf_token() }}">
                               <div class="col-lg-6 col-xlg-6 col-md-6 col-sm-6 col-sm-6">
                                 <label>Choose Week<span style="color:red;">*</span></label>
                                 <select name="week" class="form-control form-control-line test_reason" required>
-
                                   @foreach ($data['weeks'] as $key => $week)
-
                                    <option value="{{$key}}"
                                    @if($data['week'] == $key)
                                    selected
@@ -42,8 +39,22 @@
                                   @endforeach
                                 </select>
                               </div>
+                              <div class="col-lg-6 col-xlg-6 col-md-6 col-sm-6 col-sm-6">
+                                <label>Result<span style="color:red;">*</span></label>
+                                <select name="result" class="form-control form-control-line" required>
+                                  <option value="">--Select--</option>
+                                  @foreach ($data['result_data'] as $key => $result_data)
+                                  <option value="{{$key}}"
+                                  @if($data['result_data'] == $key)
+                                  selected
+                                  @endif
+                                  >{{$result_data}}</option>
+                                 @endforeach                                                                    
+                                </select>
+                                <input type="hidden" name="log_ids" value="" id="log_ids" class="form-control form-control-line" />
+                              </div>
                               <div class="col-lg-6 col-xlg-6 col-md-6 col-sm-6 col-sm-6 pull-right">
-                                <input type="submit" class="btn btn-info btn-sm" />
+                                <input type="submit" value="Submit" class="btn btn-info btn-sm" />
                               </div>
                             </form>
                           </div>
@@ -56,6 +67,7 @@
                                   <table id="exampl" class="table table-striped table-bordered responsive col-xlg-12" cellspacing="0" width="100%">
                                       <thead>
                                         <tr>
+                                          <th><input type="checkbox" id="bulk-select-all"></th>
                                           <th>Sample ID</th>
                                           <th>Enroll ID</th>
                                           <th>Action</th>
@@ -65,6 +77,12 @@
 
                                         @foreach ($data['sample'] as $key=> $samples)
                                         <tr>
+                                          <td>
+                                            @if($samples->status==-1 || $samples->status==0)                                                                                        
+                                            @elseif($samples->status>=1)
+                                            <input class="bulk-selected" type="checkbox" name="smpl_log[]" id="smpl_log_{{ $samples->log_id }}" value="{{ $samples->log_id }}">
+                                            @endif
+                                          </td>
                                           <td>{{$samples->samples}}</td>
                                           <td>{{$samples->enroll_label}}</td>
                                           <td>
@@ -84,7 +102,7 @@
                             </div>
                         </div>
                     </div>
-
+                  </form>
                 </div>
 
             </div>
@@ -141,6 +159,54 @@ $(document).ready(function() {
         ],
         "order": [[ 1, "desc" ]]
     });
+
+    var $bulk_checkboxes = $('.bulk-selected');
+    var $bulk_select_all_checkbox = $('#bulk-select-all');
+    
+
+
+        // Automatically Check or Uncheck "all select" checkbox
+        // based on the state of checkboxes in the list.
+        $bulk_checkboxes.click(function(){
+          
+            if( $bulk_checkboxes.length === $bulk_checkboxes.filter(':checked').length ){
+                $bulk_select_all_checkbox.prop('checked', true);                
+
+            }
+
+            var selected = [];
+                var $checkboxes = $('.bulk-selected:checked');           
+                $("#log_ids").val("");
+
+                      $checkboxes.each(function(i, e){
+                      selected.push( $(e).val() );
+                      // Last iteration of the loop.
+                      if( i === $checkboxes.length - 1 ){
+                          $("#log_ids").val( selected.join(',') );
+                      }
+                  });
+        });
+
+
+        // Check or Uncheck checkboxes based on the state
+        // of "all select" checkbox.
+        $bulk_select_all_checkbox.click(function(){
+            var checked = $(this).prop('checked');
+            $('.bulk-selected').prop('checked', checked);
+
+            var selected = [];
+            var $checkboxes = $('.bulk-selected:checked');           
+            $("#log_ids").val("");
+
+                  $checkboxes.each(function(i, e){
+                  selected.push( $(e).val() );
+                  // Last iteration of the loop.
+                  if( i === $checkboxes.length - 1 ){
+                      $("#log_ids").val( selected.join(',') );
+                  }
+              });
+        });
+        
 } );
 </script>
 
